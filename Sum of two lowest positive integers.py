dd319465dcc9141be60df3e39259115ec13b9e28
ninachelysheva <nccccc17@gmail.com>
2020-04-02 14:20:13 +0300

    -

--- Sum of two lowest positive integers.py
+++ Sum of two lowest positive integers.py
@@ -1,3 +1,5 @@
 def sum_two_smallest_numbers(numbers):
-    numbers.sort()
-    return numbers[0] + numbers[1]
+    new_array = [min(numbers)]
+    numbers.remove(min(numbers))
+    new_array.append(min(numbers))
+    return new_array[0] + new_array[1]
