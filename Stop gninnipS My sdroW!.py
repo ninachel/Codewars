a06b67c258f822e443160dce0cd5b445bae62b52
ninachelysheva <nccccc17@gmail.com>
2020-04-02 15:04:16 +0300

    -

---
+++ Stop gninnipS My sdroW!.py
@@ -0,0 +1,13 @@
+def spin_words(sentence):
+    sentence_array = sentence.split()
+    answer = []
+    for i in sentence_array:
+        if len(i) >= 5:
+            answer.append(i[::-1])
+        else:
+            answer.append(i)
+    return ' '.join(answer)
+
+
+sentence = 'Hey fellow warriors'
+print(spin_words(sentence))
