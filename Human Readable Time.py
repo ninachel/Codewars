524e45b2c0ddae7db7840ed7d2386a02aa384a7d
ninachelysheva <nccccc17@gmail.com>
2020-04-09 00:05:24 +0300

    -

--- Human Readable Time.py
+++ Human Readable Time.py
@@ -2,10 +2,10 @@ def make_readable(seconds):
     HH = seconds // 3600
     MM = (seconds % 3600) // 60
     SS = (seconds % 3600) % 60
-    if HH < 9:
+    if HH < 10:
         HH = str(HH).zfill(2)
-    if MM < 9:
+    if MM < 10:
         MM = str(MM).zfill(2)
-    if SS < 9:
+    if SS < 10:
         SS = str(SS).zfill(2)
     return '{}:{}:{}'.format(HH, MM, SS)
